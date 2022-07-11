9bbe2b85bb35472dd0206b7f944046a60576a404
minseo0214 <minseo>
2022-07-11 11:25:11 +0900

    body parser  만들기

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -4,15 +4,48 @@ import './styles.css'
 //CRUD (Create Read Update Delete)
 export default function App() {
   // 회원 가입 페이지
+  const [name, setName] = React.useState('')
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+
+  // 정상적인 이메일 포맷인지 확인
+  const isEmail = (email) => {
+    const emailRegex =
+      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i
+
+    return emailRegex.test(email)
+  }
+  // 비밀번호 보안상 적절한지 확인
+  // 비밀번호와 비밀번호 확인란이 동일해야함
+  // 비밀번호 가리기
+  // 비밀번호 DB에 저장할 때는 암호화
+
+  const createUser = async (id: number) => {
+    await fetch(`/${id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        alert('생성되었습니다.')
+        // history.push(`/`)
+      }
+    })
+    // post json으로 보내면 됨
+  }
+
   const register = () => {
-    return '!!!'
     return (
       <div className="container">
         Welcome
         {/* username */}
         <div>
           username
-          <input placeholder="Eneter username"></input>
+          <input placeholder="Eneter username" id="name"></input>
         </div>
         {/* email */}
         <div>
@@ -29,6 +62,7 @@ export default function App() {
           Confirm Password
           <input placeholder="Enter password again"></input>
         </div>
+        <button onClick={() => createUser(1)}> submit </button>
       </div>
     )
   }
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -2,20 +2,26 @@ import * as fs from 'fs'
 import Koa from 'koa'
 import Router from 'koa-router'
 import serve from 'koa-static'
+import bodyParser from 'koa-bodyparser'
 import { createPool, sql } from 'slonik'
 
 const app = new Koa()
+app.use(bodyParser())
 const router = new Router()
 
 const pool = createPool('postgresql://todo_user:todo_user@localhost/todos2')
 pool.connect(async (connection) => {
   const data = await connection.query(sql`SELECT * FROM todo`)
-  console.log(data)
 })
 
 router.get('/', async (ctx) => {
   const html = await fs.promises.readFile('./src/client/index.html', 'utf-8')
   ctx.body = html
+})
+
+router.post('/:id', async (ctx) => {
+  console.log(ctx.request.body)
+  ctx.status = 200
 })
 
 app.use(serve('./build'))
